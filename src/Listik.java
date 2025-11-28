097816c2582ee0c2a5ff1c243bcd0c8bfdac0fe2
zitek2 <filipzitek@seznam.cz>
2025-11-28 09:42:12 +0100

    add new class Listik with add method

---
+++ src/Listik.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Listik implements Iterable<Integer>{
+
+    private ArrayList<Integer> listik;
+
+
+    public void addNumber(int number){
+        listik.add(number);
+    }
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+}
